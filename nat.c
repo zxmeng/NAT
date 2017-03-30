6a17b8ebb069a019189cb6d70c2e2ed5d9a3ccd4
zxmeng <lucasmeng@Lucass-MacBook-Pro.local>
2017-03-30 20:51:18 +0800

    debug

--- nat.c
+++ nat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <checksum.h>
+#include "checksum.h"
 #include <netinet/in.h>
 #include <linux/netfilter.h>  
 #include <libnetfilter_queue/libnetfilter_queue.h>
